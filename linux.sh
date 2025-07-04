026cdcb455318945a8d892f7f2526c10ebc2e5f1
ArchLexor <nabindkl260@gmail.com>
2025-07-04 19:07:50 +0545

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+# extract wifi passwords on linux using bash 
+#! /bin/bash
 
 # Loop through all saved Wi-Fi configuration files
 for i in /etc/NetworkManager/system-connections/*; do
 	
-    # Read each file with sudo (since most are root-owned), and extract SSID and password using awk
+    # Read each file with sudo, and extract SSID and password using awk
     sudo cat "$i" | awk -F = '
-        /^ssid=/ {ssid = $2}       # If the line starts with 'ssid=', save the value to variable 'ssid'
-        /^psk=/  {psk  = $2}       # If the line starts with 'psk=', save the value to variable 'psk'
-        END {                     # At the end of the file, print the results
-            print "SSID: " ssid "\npassword: " psk "\n"
-        }
-    '
-
+        /^ssid=/ {ssid = $2}    
+        /^psk=/  {psk  = $2}       
+        END 
+	{print "SSID: " ssid "\npassword: " psk "\n"}'
 done
