9304f83462c362b72cef6d2c9741a801ea5d2dc4
ArchLexor <nabindkl260@gmail.com>
2025-07-04 13:41:07 +0545

    Add files via upload

---
+++ extract_passwords.ps1
@@ -0,0 +1,14 @@
+﻿# Retrieves all saved Wi-Fi SSIDs and stores them in $ssids
+$ssids = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
+
+foreach($ssid in $ssids){
+
+    # Retrieves the saved Wi-Fi password for each SSID
+    $password = (netsh wlan show profiles name=$SSID key=clear | Select-String 'Key Content') -replace ".*:\s+"
+
+    # Creates a custom object (hashtable-like) containing the SSID and its corresponding password
+    [PSCustomObject]@{
+        SSID = $ssid
+        Password = $password
+    }
+}
